32037301c91dd6d6f6544409e32f4deb139ba0fc
Jay210305 <94190939+Jay210305@users.noreply.github.com>
2023-08-22 20:00:27 -0500

    Add files via upload

---
+++ Py_Lab01/src/Principal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public Principal() {
+	}
+
+	public static void main(String[] args) {
+
+		int vida = 0,rp = 0;
+		boolean fl = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			vida++;
+			System.out.println("Vida"+vida+"\tCual es el numero secreto?");
+			rp = sc.nextInt();
+			if (rp == 1234)
+				fl = true;
+		}while (vida<3 && !fl);
+		if (fl = true)
+			System.out.println("Adivinaste!!");
+		else
+			System.out.println("ups, perdiste ... :(");
+
+	}
+
+}
